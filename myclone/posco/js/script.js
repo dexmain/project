e2eef19e695dbb9d0768b3881fa979636da1710b
jhwmac <jkr2505@naver.com>
2022-02-08 18:34:11 +0900

    헤더 색상

--- myclone/posco/js/script.js
+++ myclone/posco/js/script.js
@@ -1,36 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // 위로가기 기능
     let gotop = $('.gotop');
-    gotop.click(function(){
+    gotop.click(function () {
         $('html, body').stop().animate({
             scrollTop: 0
         }, 400);
     });
 
     // 위로가기 사라짐 효과
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         // 스크롤 바가 이동한 거리 체크
         let sc = $(window).scrollTop();
-        if(sc >= 50) {
+        if (sc >= 50) {
             gotop.addClass('gotop-active');
         } else {
             gotop.removeClass('gotop-active');
         }
     });
 });
-$(function(){
+$(function () {
     var $header = $('header'); //헤더를 변수에 넣기
     var $page = $('.header-point'); //색상이 변할 부분
     var $window = $(window);
-    var pageOffsetTop = $page.offset().top;//색상 변할 부분의 top값 구하기
-    
-    $window.resize(function(){ //반응형을 대비하여 리사이즈시 top값을 다시 계산
+
+    var pageOffsetTop = $page.offset().top; //색상 변할 부분의 top값 구하기
+    $window.resize(function () { //반응형을 대비하여 리사이즈시 top값을 다시 계산
         pageOffsetTop = $page.offset().top;
     });
 
-    $window.on('scroll', function(){ //스크롤시
-      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
-      $header.toggleClass('down', scrolled); //클래스 토글
+
+    $window.on('scroll', function () { //스크롤시
+        var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false 
+        var mode = document.getElementById("modechanger");
+        var modeval = mode.value;
+        if(modeval == 'true'){
+            $header.toggleClass('down', scrolled); //클래스 토글
+            console.log(modeval);
+        }
+        else {
+            $header.toggleClass('dark-down', scrolled); //클래스 토글
+            console.log(modeval);
+        }
     });
+
 });
+
+function Buttontoggle() {
+    var mode = document.getElementById("modechanger");
+    var modeval = mode.value;
+    if (modeval == 'true') {
+        mode.setAttribute('value', 'false');
+        mode.style.backgroundimage = "url(day.png)"
+    } else {
+        mode.setAttribute('value', 'true');
+    }
+}

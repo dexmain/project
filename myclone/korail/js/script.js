57006cab59a3c1a66fb1a44fc814585eb6e1ebcf
jhwmac <jkr2505@naver.com>
2022-02-21 11:43:41 +0900

    gnb li hover js > css

--- myclone/korail/js/script.js
+++ myclone/korail/js/script.js
@@ -9,30 +9,30 @@ $(window).ready(function () {
     })
 
     // nav 벗어 나기 전 까지 gnb>li>a 포커스 유지
-    gnb_li.eq(0).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(0).children('a').addClass('gnb-a-focus');
-    })
-    gnb_li.eq(1).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(1).children('a').addClass('gnb-a-focus');
-    })
-    gnb_li.eq(2).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(2).children('a').addClass('gnb-a-focus');
-    })
-    gnb_li.eq(3).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(3).children('a').addClass('gnb-a-focus');
-    })
-    gnb_li.eq(4).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(4).children('a').addClass('gnb-a-focus');
-    })
-    gnb_li.eq(5).mouseenter(function(){
-        gnb_a.removeClass('gnb-a-focus');
-        gnb_li.eq(5).children('a').addClass('gnb-a-focus');
-    })
+    // gnb_li.eq(0).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(0).children('a').addClass('gnb-a-focus');
+    // })
+    // gnb_li.eq(1).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(1).children('a').addClass('gnb-a-focus');
+    // })
+    // gnb_li.eq(2).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(2).children('a').addClass('gnb-a-focus');
+    // })
+    // gnb_li.eq(3).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(3).children('a').addClass('gnb-a-focus');
+    // })
+    // gnb_li.eq(4).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(4).children('a').addClass('gnb-a-focus');
+    // })
+    // gnb_li.eq(5).mouseenter(function(){
+    //     gnb_a.removeClass('gnb-a-focus');
+    //     gnb_li.eq(5).children('a').addClass('gnb-a-focus');
+    // })
 
     nav.mouseleave(function(){
         gnb_a.removeClass('gnb-a-focus');

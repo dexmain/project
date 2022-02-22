d482ec4747847f0a240365d99b8dedcfa3c77054
jhwmac <jkr2505@naver.com>
2022-02-22 18:08:30 +0900

    chatbot fin

--- myclone/korail/js/script.js
+++ myclone/korail/js/script.js
@@ -25,11 +25,9 @@ $(window).ready(function () {
 
 
     let h_link_menu = $('.header-link-menu');
-    let h_link_menu_list = $('.header-link-menu-list');
     let h_link_lang = $('.header-link-lang');
-    let h_link_lang_list = $('.header-link-lang-list');
 
-    // 자주찾는 메뉴 언어전환
+    // 자주찾는 메뉴 언어 파트
     h_link_menu.click(function () {
         h_link_menu.toggleClass('header-link-menu-on');
     });
@@ -50,7 +48,6 @@ $(window).ready(function () {
     })
     // 비주얼 팝업 슬라이드 오토플레이 on/off 버튼
     let v_popup_pause = $('.sw-popup-pause');
-    let v_popup_play = $('.sw-popup-play');
     v_popup_pause.click(function () {
         $(this).toggleClass('sw-popup-play');
         if ($(this).hasClass('sw-popup-play')) {
@@ -76,25 +73,17 @@ $(window).ready(function () {
             delay: 3000,
         },
         on: {
-            activeIndexChange: function() {
+            activeIndexChange: function () {
                 let visual_control_txt_list = [
                     'KTX 산천 북한강철교 운행',
                     '임진강 철도교량 점검',
                     'KTX 승무원 고객응대',
                     '전동열차 코로나19 방역',
                 ];
-                let i = this.realIndex;
                 let visual_control_txt = $('.visual-control-txt');
-                console.log(visual_control_txt_list[i]);
+                let i = this.realIndex;
                 visual_control_txt.html('<span>' + visual_control_txt_list[i] + '</span>');
             }
         }
     });
-
-    
-
-    
-
-
-
 })

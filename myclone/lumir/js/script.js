71982f6739487dfdf8a2314f529dfbe7c241bf1c
jhwmac <jkr2505@naver.com>
2022-03-03 12:49:42 +0900

    비주얼 스토리 버튼 진행예정

--- myclone/lumir/js/script.js
+++ myclone/lumir/js/script.js
@@ -3,26 +3,56 @@ $(document).ready(function () {
     let $gnb_field = $('.gnb-field');
     let $gnb_li = $('.gnb>li');
     let $submenu = $('.submenu');
+    let $kor = $('.kor');
+    let $eng = $('.eng');
+    let $eng_area = $('.eng-area');
+    let $language = $('.language');
+    let $visual_txt = $('.visual-txt');
+
     // let $submenu_col = $('.submenu-col');
     // let $submenu_row = $('.submenu-row');
+
+
+    // li on
+    $submenu.hide();
+    $gnb_field.hide();
     $.each($gnb_li, function (index, item) {
         $gnb_li.eq(index).mouseenter(function () {
             $gnb_li.removeClass('li-on');
             $(this).addClass('li-on');
-            $submenu.find('li').stop().fadeOut(300);
-            $submenu.eq(index).find('li').stop().fadeIn(300);
-            if(index == 0){
-                $gnb_field.css('height',210);
-            }
-            else{
-                $gnb_field.css('height',72);
+            $gnb_field.fadeIn(300);
+            $submenu.stop().fadeOut(300);
+            $submenu.eq(index).stop().fadeIn(300);
+            if (index == 0) {
+                $gnb_field.css('height', 210);
+            } else {
+                $gnb_field.css('height', 72);
             }
 
         });
     });
-    $header.mouseleave(function(){
-        $gnb_li.removeClass('li-on');
-        $gnb_field.css('height',0);
+    // li off
+    $header.mouseleave(function () {
+        $submenu.hide();
+        $gnb_field.fadeOut(300);
+        $gnb_field.css('height', 0);
+
     })
 
+    // 언어 모드 변경 
+    $kor.click(function () {
+        $language.toggleClass('lang-active');
+    })
+
+    // 헤더 스크롤
+    $(window).scroll(function () {
+        let sc = $(window).scrollTop();
+        let bool = sc >= 50;
+        
+        $header.toggleClass('down',bool);
+    });
+    function visual_txt(){
+        $visual_txt.addClass('txt-up');
+    }
+    visual_txt();
 })

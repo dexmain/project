da585612ed809dafd7b7bb8c4c409503ed8ea54d
jhwmac <jkr2505@naver.com>
2022-03-16 14:13:35 +0900

    proflie frame setting

--- profile/js/script.js
+++ profile/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
-    var swiper = new Swiper(".wrap-swiper", {
+    
+    // wrap-slide
+    let menu = ['home', 'profile', 'portfolio','skill','MBTI','contact']
+    let swiper = new Swiper(".wrap-swiper", {
         slidesPerView: 1,
         mousewheel: true,
-        speed:500,
+        speed: 500,
         pagination: {
-            el: ".swiper-pagination",
+            el: ".page-box",
             clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (menu[index]) + '</span>';
+            },
         },
+
     });
 })

602361f943c6e7622316d1e89b94bd6ce8d5d73c
jhwmac <jkr2505@naver.com>
2022-03-17 22:42:41 +0900

    tag add

--- profile/js/script.js
+++ profile/js/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    
-    // wrap-slide
-    let menu = ['home', 'profile', 'portfolio','skill','MBTI','contact']
-    let swiper = new Swiper(".wrap-swiper", {
-        effect:'fade',
+
+    // 전체화면 슬라이드
+    let menu = ['home', 'profile', 'portfolio', 'skill', 'MBTI', 'contact']
+    let wrap_swiper = new Swiper(".wrap-swiper", {
+        effect: 'fade',
         slidesPerView: 1,
         mousewheel: true,
+        touchRatio: 0,
         speed: 500,
         pagination: {
             el: ".page-box",
@@ -14,6 +15,33 @@ $(document).ready(function () {
                 return '<span class="' + className + '">' + (menu[index]) + '</span>';
             },
         },
-
     });
+    // 포트폴리오 슬라이드
+    var portfolio_swiper = new Swiper(".portfolio-swiper", {
+        slidesPerView: 1,
+        loopAdditionalSlides: 1,
+        loop: true,
+        speed: 300,
+        centeredSlides: true,
+        spaceBetween: 50,
+        navigation: {
+            nextEl: ".sw-portfolio-next",
+            prevEl: ".sw-portfolio-prev",
+        },
+    });
+
+    let $port_next = $('.sw-portfolio-next');
+    let $port_prev = $('.sw-portfolio-prev');
+    let $gear_img = $('.gear img');
+    let angle = 0; // 현재의 각도를 변수로 저장
+    $port_prev.click(function () {
+        angle += +90;
+        $gear_img.rotate(angle);
+    });
+    
+    $port_next.click(function () {
+        angle += -90;
+        $gear_img.rotate(angle);
+    });
+
 })
